2b933cb0d4de35854268e4045e33db4979f5c0b5
unknown <blossooom@163.com>
2023-12-26 17:22:30 +0800

    clean up the code

--- src/component/convert/fragments/QRCodeGenerator.tsx
+++ src/component/convert/fragments/QRCodeGenerator.tsx
@@ -10,29 +10,22 @@ export default function qrCodeGenerator() {
   const [areaText, setAreaText] = useState('');
   const [fgColor, setFgColor] = useState('#000000');
   const [bgColor, setBgColor] = useState('#FFF');
-  const [showQRcode, setShowQRcode] = useState(''); //show qrcode
-  const [hidden, setHidden] = useState(false); //show color picker
-  const [bghidden, setBgHidden] = useState(false); //show color picker
-  const [shidden, setSHidden] = useState(false); //show size change
-  const [chsize, setChSize] = useState(220); //size
+  const [showQRcode, setShowQRcode] = useState('');
+  const [hidden, setHidden] = useState(false);
+  const [bghidden, setBgHidden] = useState(false);
+  const [sizeHidden, setSizeHidden] = useState(false);
+  const [chsize, setChSize] = useState(220);
   const [menu, setMenu] = useState('link');
-  const [showSpin, setShowSpin] = useState(false); //show spin
-  const [imageSettings, setImageSettings] = useState({
-    src: '',
-    x: 56,
-    y: 0,
-    height: 24,
-    width: 24,
-    excavate: true,
-  });
+  const [showSpin, setShowSpin] = useState(false);
 
   const { TextArea } = Input;
+  const defaultValue = 220;
 
   useEffect(() => {
-    if (showQRcode && hidden && chsize && imageSettings) {
+    if (showQRcode && hidden && chsize) {
       setShowQRcode('');
     }
-  }, [showQRcode, hidden, chsize, imageSettings]);
+  }, [showQRcode, hidden, chsize]);
 
   const antIcon = <LoadingOutlined style={{ fontSize: 30 }} spin />;
 
@@ -53,7 +46,7 @@ export default function qrCodeGenerator() {
     if (menu === 'link') {
       if (chsize < 0) {
         setShowQRcode(text);
-        message.error('size cannot be minus！');
+        message.error('The size cannot be minus！');
       } else if (chsize > 0 && chsize <= 30) {
         setShowQRcode(text);
         message.warning('Generate QR code size is too small.You need modify a more appropriate value!');
@@ -75,22 +68,18 @@ export default function qrCodeGenerator() {
   function onColorChangeHandler(color: { hex: string }) {
     setFgColor(color.hex);
     setHidden(false);
-    console.log('color', color);
   }
 
   function onBGColorChangeHandler(color: { hex: string }) {
     setBgColor(color.hex);
     setBgHidden(false);
-    console.log('BGcolor', color);
   }
 
   function onTextAreaValueChangeHandler(textareaValue: React.ChangeEvent<HTMLTextAreaElement>) {
-    console.log(textareaValue.target);
     setAreaText(textareaValue.target.value);
   }
 
   function onInputValueChangeHandler(linkValue: React.ChangeEvent<HTMLInputElement>) {
-    console.log(linkValue.target.value);
     setText(linkValue.target.value);
   }
 
@@ -101,17 +90,13 @@ export default function qrCodeGenerator() {
     setBgHidden(false);
   }
 
-  function onBlurHandler(e) {
-    console.log('blur->', e);
-  }
-
   function onSizeChangeHandler(e) {
     setShowQRcode('');
     setChSize(e.target.value);
     if (e.target.value === '') {
       setChSize(220);
-      setShowQRcode('');
-    } //default & empty = 220
+      setShowQRcode('');//input type为数字时 监听值的类型是什么
+    }
     console.log('sizechange->', e.target.value, chsize);
   }
 
@@ -142,7 +127,7 @@ export default function qrCodeGenerator() {
         downloadFunc = htmlToImage.toPng;
         extension = 'png';
       } else {
-        message.error('Invalid download type');
+        message.error('Invalid download type.');
         return;
       }
       downloadFunc(qrcodeImage)
@@ -153,7 +138,7 @@ export default function qrCodeGenerator() {
           link.click();
         })
         .catch((error) => {
-          console.error('Error gernarating ${extension}:', error);
+          console.error('Error gernerating ${extension}:', error);
         });
     }
   }
@@ -162,11 +147,11 @@ export default function qrCodeGenerator() {
     <div className="wrap-grid">
       <div>
         <Menu mode="vertical" selectedKeys={[menu]} onClick={onMenuItemClickHandler}>
-          <Menu.Item key="link" style={{ backgroundColor: '#F8F8F8', textAlign: 'center' }}>
+          <Menu.Item key="link" className="text-c menu-item-bg">
             <LinkOutlined />
             <label className="margin-l5">Link</label>
           </Menu.Item>
-          <Menu.Item key="text" style={{ backgroundColor: '#F8F8F8', textAlign: 'center' }}>
+          <Menu.Item key="text" className="text-c menu-item-bg">
             <AlignLeftOutlined />
             <label className="margin-l5">Text</label>
           </Menu.Item>
@@ -174,16 +159,16 @@ export default function qrCodeGenerator() {
       </div>
       <div>
         {menu === 'link' && (
-          <div style={{ backgroundColor: '#f5f5ff' }}>
+          <div className="content-bg">
             <h2>Link</h2>
             <div className="margin-b10">
               <label>URL</label>
-              <Input placeholder="type a http://" value={text} allowClear onBlur={onBlurHandler} onChange={onInputValueChangeHandler} />
+              <Input placeholder="type a http://" value={text} allowClear onChange={onInputValueChangeHandler} />
             </div>
           </div>
         )}
         {menu === 'text' && (
-          <div style={{ backgroundColor: '#f5f5ff' }}>
+          <div className="content-bg">
             <h2>Text</h2>
             <div className="margin-b10">
               <label>Message</label>
@@ -191,63 +176,45 @@ export default function qrCodeGenerator() {
             </div>
           </div>
         )}
-        <Button
-          style={{
-            color: '#FFFFFF',
-            paddingLeft: 10,
-            paddingTop: 2,
-            backgroundColor: '#2196e3',
-            border: '#2196e3',
-            borderRadius: 5,
-            textAlign: 'center',
-          }}
-          className="timestamp-button margin-b10"
-          onClick={handleConvert}
-        >
+        <Button className="timestamp-button margin-b10 qrcode-btn qrcode-btn-p" onClick={handleConvert}>
           generate your new qrcode
         </Button>
         <Button onClick={() => setHidden(!hidden)}>Choose qrcode Color</Button>
-        <div style={{ display: 'flex' }}>
+        <div className="flex">
           <label>current fgcolor</label>
-          <div
-            className="colorbox-shadow"
-            style={{ width: 24, height: 24, backgroundColor: fgColor, border: '1px solid #ccc', display: 'inline-block', padding: '5px' }}
-          ></div>
+          <div className="colorbox colorbox-shadow pd5" style={{ backgroundColor: fgColor }}></div>
           {hidden && (
-            <div className="subitem-bg" style={{ position: 'absolute', zIndex: '20' }}>
+            <div className="subitem-bg position-a index20 pd10">
               <SketchPicker color={fgColor} onChange={onColorChangeHandler} display={hidden ? 'block' : 'none'}></SketchPicker>
             </div>
           )}
         </div>
         <Button onClick={() => setBgHidden(!bghidden)}>Choose qrcode bg Color</Button>
-        <div style={{ display: 'flex' }}>
+        <div className="flex">
           <label>current bgcolor</label>
-          <div
-            className="colorbox-shadow"
-            style={{ width: 24, height: 24, backgroundColor: bgColor, border: '1px solid #ccc', display: 'inline-block' }}
-          ></div>
+          <div className="colorbox colorbox-shadow pd5" style={{ backgroundColor: bgColor }}></div>
           {bghidden && (
-            <div className="subitem-bg" style={{ position: 'absolute', zIndex: '20' }}>
+            <div className="subitem-bg position-a index20 pd10">
               <SketchPicker color={bgColor} onChange={onBGColorChangeHandler} display={bghidden ? 'block' : 'none'}></SketchPicker>
             </div>
           )}
         </div>
-        <Button onClick={() => setSHidden(!shidden)}>change size</Button>
-        {shidden && (
-          <div className="subitem-bg" style={{ width: 350 }}>
+        <Button onClick={() => setSizeHidden(!sizeHidden)}>change size</Button>
+        {sizeHidden && (
+          <div className="subitem-bg pd10" style={{ width: 350 }}>
             <Input
               placeholder="type value(number) change size"
-              value={220 ? undefined : chsize}
+              value={defaultValue ? undefined : chsize}
               type="number"
               allowClear
               onChange={onSizeChangeHandler}
             />
             <div className="margin-t10">
               <Select
-                defaultValue={220}
+                defaultValue={defaultValue}
                 onChange={onSelectChangeHandler}
                 options={[
-                  { value: 220, label: ' 默认尺寸' },
+                  { value: defaultValue, label: ' 默认尺寸' },
                   { value: 50, label: 50 },
                   { value: 100, label: 100 },
                   { value: 200, label: 200 },
@@ -259,31 +226,30 @@ export default function qrCodeGenerator() {
             </div>
           </div>
         )}
-        <div
-          className="div-flex h-center v-middle"
-          style={{ border: '1px solid #2196e3', backgroundColor: '#f2f8fd', width: chsize + 20, height: chsize + 20 }}
-        >
+        <div className="div-flex h-center v-middle qrcode-box" style={{ width: chsize + 20, height: chsize + 20 }}>
           {showSpin && <Spin indicator={antIcon} />}
           {showQRcode && (
             <QRCodeSVG
+              className="pd10"
               id="qrcode-svg"
-              style={{ maxHeight: '100%', maxWidth: '100%', padding: 10 }}
+              style={{ maxHeight: '100%', maxWidth: '100%' }}
               value={showQRcode}
               size={chsize}
               bgColor={bgColor}
               fgColor={fgColor}
               level={'L'}
               includeMargin={false}
-              imageSettings={imageSettings}
+              imageSettings={{ src: '', x: 56, y: 0, height: 24, width: 24, excavate: true }}
             />
           )}
         </div>
         <div>
-          <label style={{ fontSize: 12, color: 'red' }}>注: 生成二维码的默认尺寸是 220*220</label>
+          <label className="fontSize12 c-red">注: 生成二维码的默认尺寸是 220*220</label>
         </div>
         <div>
           <Button
-            style={{ backgroundColor: '#2196e3', marginRight: 8, marginTop: 8, color: '#fff' }}
+            className="qrcode-btn"
+            style={{ marginRight: 8, marginTop: 8 }}
             value={'svg'}
             onClick={() => {
               onDownloadQRCodeImageHandler('svg');
@@ -292,7 +258,7 @@ export default function qrCodeGenerator() {
             Download SVG
           </Button>
           <Button
-            style={{ backgroundColor: '#2196e3', color: '#fff' }}
+            className="qrcode-btn"
             value={'png'}
             onClick={() => {
               onDownloadQRCodeImageHandler('png');
@@ -300,7 +266,6 @@ export default function qrCodeGenerator() {
           >
             Download PNG
           </Button>
-          {/* <Button onClick={onDownloadQRCodeImageHandler}>download qrcode</Button> */}
         </div>
       </div>
     </div>
